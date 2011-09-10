9068a4921082e919c9f34d8a07344663878a213d
Jonas Genannt <jonas@brachium-system.net>
2011-09-10 12:14:01 +0200

    strip also stuff to build activemq rpm package contains stuff from activemq upstream

--- debian/repackage.sh
+++ debian/repackage.sh
@@ -27,6 +27,10 @@ rm -rf ${TEMP_DIR}/mcollective-${MC_VERSION}/lib/mcollective/vendor/systemu
 rm -rf ${TEMP_DIR}/mcollective-${MC_VERSION}/lib/mcollective/vendor/load_json.rb
 rm -rf ${TEMP_DIR}/mcollective-${MC_VERSION}/lib/mcollective/vendor/load_systemu.rb
 
+# strip included stuff to build ActiveMQ rpm package
+rm -rf ${TEMP_DIR}/mcollective-${MC_VERSION}/ext/activemq/wlcg-patch.tgz
+rm -rf ${TEMP_DIR}/mcollective-${MC_VERSION}/ext/activemq/apache-activemq.spec
+
 cd ${TEMP_DIR}
 tar cvz mcollective-${MC_VERSION} -f ${CURDIR}/mcollective_${MC_VERSION}+dfsg.orig.tar.gz
 cd ${CURDIR}
