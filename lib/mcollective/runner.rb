2f178cde0914c5157b468f0b7678ed9e69408028
Jonas Genannt <jonas.genannt@capi2name.de>
2012-05-05 12:47:57 +0200

    Adding upstream version 2.0.0+dfsg.

--- lib/mcollective/runner.rb
+++ lib/mcollective/runner.rb
@@ -1,158 +1,132 @@
 module MCollective
-    # The main runner for the daemon, supports running in the foreground
-    # and the background, keeps detailed stats and provides hooks to access
-    # all this information
-    class Runner
-        def initialize(configfile)
-            @config = Config.instance
-            @config.loadconfig(configfile) unless @config.configured
+  # The main runner for the daemon, supports running in the foreground
+  # and the background, keeps detailed stats and provides hooks to access
+  # all this information
+  class Runner
+    def initialize(configfile)
+      @config = Config.instance
+      @config.loadconfig(configfile) unless @config.configured
 
-            @stats = PluginManager["global_stats"]
+      @stats = PluginManager["global_stats"]
 
-            @security = PluginManager["security_plugin"]
-            @security.initiated_by = :node
+      @security = PluginManager["security_plugin"]
+      @security.initiated_by = :node
 
-            @connection = PluginManager["connector_plugin"]
-            @connection.connect
+      @connection = PluginManager["connector_plugin"]
+      @connection.connect
 
-            @agents = Agents.new
+      @agents = Agents.new
 
-            Signal.trap("USR1") do
-                Log.info("Reloading all agents after receiving USR1 signal")
-                @agents.loadagents
-            end
-
-            Signal.trap("USR2") do
-                Log.info("Cycling logging level due to USR2 signal")
-                Log.cycle_level
-            end
+      unless Util.windows?
+        Signal.trap("USR1") do
+          Log.info("Reloading all agents after receiving USR1 signal")
+          @agents.loadagents
         end
 
-        # Daemonize the current process
-        def self.daemonize
-            fork do
-                Process.setsid
-                exit if fork
-                Dir.chdir('/tmp')
-                STDIN.reopen('/dev/null')
-                STDOUT.reopen('/dev/null', 'a')
-                STDERR.reopen('/dev/null', 'a')
-
-                yield
-            end
+        Signal.trap("USR2") do
+          Log.info("Cycling logging level due to USR2 signal")
+          Log.cycle_level
         end
-
-        # Starts the main loop, before calling this you should initialize the MCollective::Config singleton.
-        def run
-            controltopics = Util.make_target("mcollective", :command)
-            Util.subscribe(controltopics)
-
-            # Start the registration plugin if interval isn't 0
-            begin
-                PluginManager["registration_plugin"].run(@connection) unless @config.registerinterval == 0
-            rescue Exception => e
-                Log.error("Failed to start registration plugin: #{e}")
-            end
-
-            loop do
-                begin
-                    msg = receive
-
-                    collective = msg[:collective]
-                    agent = msg[:agent]
-
-                    # requests from older clients would not include the
-                    # :collective and :agent this parses the target in
-                    # a backward compat way for them
-                    unless collective && agent
-                        parsed_dest = Util.parse_msgtarget(msg[:msgtarget])
-                        collective = parsed_dest[:collective]
-                        agent = parsed_dest[:agent]
-                    end
-
-                    if agent == "mcollective"
-                        Log.debug("Handling message for mcollectived controller")
-
-                        controlmsg(msg, collective)
-                    else
-                        Log.debug("Handling message for agent '#{agent}' on collective '#{collective}'")
-
-                        agentmsg(msg, agent, collective)
-                    end
-                rescue Interrupt
-                    Log.warn("Exiting after interrupt signal")
-                    @connection.disconnect
-                    exit!
-
-                rescue NotTargettedAtUs => e
-                    Log.debug("Message does not pass filters, ignoring")
-
-                rescue Exception => e
-                    Log.warn("Failed to handle message: #{e} - #{e.class}\n")
-                    Log.warn(e.backtrace.join("\n\t"))
-                end
-            end
-        end
-
-        private
-        # Deals with messages directed to agents
-        def agentmsg(msg, target, collective)
-            @agents.dispatch(msg, target, @connection) do |replies|
-                dest = Util.make_target(target, :reply, collective)
-                reply(target, dest, replies, msg[:requestid], msg[:callerid]) unless replies == nil
-            end
-        end
-
-        # Deals with messages sent to our control topic
-        def controlmsg(msg, collective)
-            begin
-                body = msg[:body]
-                requestid = msg[:requestid]
-                callerid = msg[:callerid]
-
-                replytopic = Util.make_target("mcollective", :reply, collective)
-
-                case body
-                    when /^stats$/
-                        reply("mcollective", replytopic, @stats.to_hash, requestid, callerid)
-
-                    when /^reload_agent (.+)$/
-                        reply("mcollective", replytopic, "reloaded #{$1} agent", requestid, callerid) if @agents.loadagent($1)
-
-                    when /^reload_agents$/
-                        reply("mcollective", replytopic, "reloaded all agents", requestid, callerid) if @agents.loadagents
-
-                    else
-                        Log.error("Received an unknown message to the controller")
-
-                end
-            rescue Exception => e
-                Log.error("Failed to handle control message: #{e}")
-            end
-        end
-
-        # Receive a message from the connection handler
-        def receive
-            msg = @connection.receive
-
-            @stats.received
-
-            msg = @security.decodemsg(msg)
-
-            raise(NotTargettedAtUs, "Received message is not targetted to us")  unless @security.validate_filter?(msg[:filter])
-
-            msg
-        end
-
-        # Sends a reply to a specific target topic
-        def reply(sender, target, msg, requestid, callerid)
-            reply = @security.encodereply(sender, target, msg, requestid, callerid)
-
-            @connection.send(target, reply)
-
-            @stats.sent
-        end
+      else
+        Util.setup_windows_sleeper
+      end
     end
+
+    # Starts the main loop, before calling this you should initialize the MCollective::Config singleton.
+    def run
+      Util.subscribe(Util.make_subscriptions("mcollective", :broadcast))
+      Util.subscribe(Util.make_subscriptions("mcollective", :directed)) if @config.direct_addressing
+
+      # Start the registration plugin if interval isn't 0
+      begin
+        PluginManager["registration_plugin"].run(@connection) unless @config.registerinterval == 0
+      rescue Exception => e
+        Log.error("Failed to start registration plugin: #{e}")
+      end
+
+      loop do
+        begin
+          request = receive
+
+          if request.agent == "mcollective"
+            controlmsg(request)
+          else
+            agentmsg(request)
+          end
+        rescue SignalException => e
+          Log.warn("Exiting after signal: #{e}")
+          @connection.disconnect
+          raise
+
+        rescue MsgTTLExpired => e
+          Log.warn(e)
+
+        rescue NotTargettedAtUs => e
+          Log.debug("Message does not pass filters, ignoring")
+
+        rescue Exception => e
+          Log.warn("Failed to handle message: #{e} - #{e.class}\n")
+          Log.warn(e.backtrace.join("\n\t"))
+        end
+      end
+    end
+
+    private
+    # Deals with messages directed to agents
+    def agentmsg(request)
+      Log.debug("Handling message for agent '#{request.agent}' on collective '#{request.collective}'")
+
+      @agents.dispatch(request, @connection) do |reply_message|
+        reply(reply_message, request) if reply_message
+      end
+    end
+
+    # Deals with messages sent to our control topic
+    def controlmsg(request)
+      Log.debug("Handling message for mcollectived controller")
+
+      begin
+        case request.payload[:body]
+        when /^stats$/
+          reply(@stats.to_hash, request)
+
+        when /^reload_agent (.+)$/
+          reply("reloaded #{$1} agent", request) if @agents.loadagent($1)
+
+        when /^reload_agents$/
+
+          reply("reloaded all agents", request) if @agents.loadagents
+
+        else
+          Log.error("Received an unknown message to the controller")
+
+        end
+      rescue Exception => e
+        Log.error("Failed to handle control message: #{e}")
+      end
+    end
+
+    # Receive a message from the connection handler
+    def receive
+      request = @connection.receive
+      request.type = :request
+
+      @stats.received
+
+      request.decode!
+      request.validate
+
+      request
+    end
+
+    # Sends a reply to a specific target topic
+    def reply(msg, request)
+      msg = Message.new(msg, nil, :request => request)
+      msg.encode!
+      msg.publish
+
+      @stats.sent
+    end
+  end
 end
 
-# vi:tabstop=4:expandtab:ai
